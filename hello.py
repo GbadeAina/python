d88b84c8cd207148483901c29bb26711d4fc4089
GbadeAina <46222365+GbadeAina@users.noreply.github.com>
2019-02-14 12:01:53 +0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+name = input("What's your name? ")
+age = int(input("Age? "))
+
+if age < 10:
+    print("Hi, little " + name + "!")
+    
+else:
+    print("Hello " + name + "!")
+
+
+print("How are you?")
